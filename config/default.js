ff85ad4202551ab3c3ad23e72b3c1e0f0850741d
Julio Cesar Maldonado <jcmaldonado@asesoftware.com>
2022-05-24 13:54:13 -0500

    feat: added docker

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  port: 3000,
+  port: process.env.PORT || 3000,
   mongodb: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
