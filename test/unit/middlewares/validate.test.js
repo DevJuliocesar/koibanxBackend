e8c06f4623d18bd1441a3e2783752ffb5fd8325f
Julio Cesar Maldonado <jcmaldonado@asesoftware.com>
2022-05-24 00:59:44 -0500

    feat: validate test

--- test/unit/middlewares/validate.test.js
+++ test/unit/middlewares/validate.test.js
@@ -1,10 +1,5 @@
 /* eslint-disable no-import-assign */
 /* eslint-disable no-undef */
-const httpMocks = require('node-mocks-http');
-const validator = require('express-validator');
-
-const validate = require('../../../middlewares/validate');
-const authValidation = require('../../../validations/auth.validation');
 
 jest.mock('express-validator', () => ({
   checkSchema: jest.fn().mockReturnValue([
@@ -15,14 +10,37 @@ jest.mock('express-validator', () => ({
   validationResult: jest.fn(() => ({ isEmpty: () => true })),
 }));
 
+const httpMocks = require('node-mocks-http');
+const { validationResult } = require('express-validator');
+
+const validate = require('../../../middlewares/validate');
+const authValidation = require('../../../validations/auth.validation');
+
 describe('Validator Middleware', () => {
-  it('calls next if there are no validation errors', async () => {
+  test('calls next if there are no validation errors', async () => {
     const request = httpMocks.createRequest();
     const response = httpMocks.createResponse();
-    const next = jest.fn(() => ({ isEmpty: () => false }));
+    const next = jest.fn();
 
     await validate(authValidation.login)(request, response, next);
     expect(response.statusCode).toBe(200);
     expect(next).toBeCalled();
+  });
+
+  test('calls errors with status 422', async () => {
+    const request = httpMocks.createRequest();
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    validationResult.mockImplementation(() => ({
+      isEmpty: jest.fn().mockReturnValue(false),
+      array: jest
+        .fn()
+        .mockReturnValue([{ test1: 'error1' }, { test2: 'error2' }]),
+    }));
+
+    await validate(authValidation.login)(request, response, next);
+    expect(response.statusCode).toBe(422);
+    expect(next).not.toBeCalled();
   });
 });
